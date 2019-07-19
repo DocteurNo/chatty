b9cf7d02f2d74c26e2d00e9a4a7fae84f4c4e219
DocteurNo <stanpub49-poubelle@yahoo.fr>
2019-07-19 13:43:31 +0200

    Flashing messages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,3 +6,9 @@ Route::get('/', [
     'uses' => 'HomeController@index',
     'as' => 'home',
 ]);
+
+Route::get('/alert', function () {
+    return redirect()
+        ->route('home')
+        ->with('info', 'You are signed up!');
+});
